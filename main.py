066d02cfdd18714c3c39dd7d7f45e755760f31bb
CalciumArgon <212245086@qq.com>
2024-06-22 10:48:28 +0800

    模拟的初始化; 对当前模拟状态的打印

--- main.py
+++ main.py
@@ -10,16 +10,102 @@ class Simulation():
     def __init__(self, cfg):
         self.config = cfg
         self.virus = Virus(cfg.virus.name, cfg.virus.infect_radius)
+
+        # 时钟信息 -------------------------------------
+        self.clock = 0  # 按分钟计 ?
+        # --------------------------------------------
+
+        # 记录所有实验室
         self.students = []
         self.teachers = []
-        self.labs = []
+        # self.labs = []
 
-        for i in range(cfg.student.number / 3):
-            self.students.append(Student(0, 'E', cfg.student.move_matrix, [0, 0]))
-            self.students.append(Student(1, 'W', cfg.student.move_matrix))
+        # 单个实验室中的人数
+        self.w_number = cfg.student.w_number
+        self.e_number = cfg.student.e_number
+        self.stu_num = cfg.student.e_number + cfg.student.w_number
+        self.tea_num = cfg.teacher.number
+
+        self.lab_num = cfg.environment.lab_number
+        self.lab_size = (cfg.environment.lab_length, cfg.environment.lab_width)
+        for lab in range(self.lab_num):
+            # 对于每个lab的学生, 全局标识号的前部分为初始在实验室的, 后部分为初始在工作区的
+            for i in range(cfg.student.e_number):
+                # 添加实验室学生, 坐标随机, 全局标识号为: lab * stu_num + i
+                self.students.append(Student(
+                                            lab * self.stu_num + i, 
+                                            lab, 'E', cfg.student.move_matrix,
+                                            # 如果整点随机改为 np.random.randint
+                                            lab_postion=(np.random.uniform(0,self.lab_size[0]), np.random.uniform(0, self.lab_size[1]))
+                                        )
+                                    )
+            for i in range(cfg.student.w_number):
+                # 添加工作区学生, 工位号固定为其全局标识号: lab * stu_num + e_number + i
+                self.students.append(Student(lab * self.stu_num + cfg.student.e_number + i, 
+                                             lab, 'W', cfg.student.move_matrix
+                                        )
+                                    )
+                
+        for lab in range(cfg.environment.lab_number):
+            for i in range(cfg.teacher.number):
+                # 老师初始都在 Office, 全局标识符: lab * cfg.teacher.number + i
+                self.teachers.append(Teacher(lab * cfg.teacher.number + i,
+                                              lab, 'O', cfg.teacher.move_matrix
+                                        )
+                                    )
+
+    @staticmethod
+    def day(self):
+        return self.clock // (24 * 60)
+    
+    @staticmethod
+    def hour(self):
+        return (self.clock // 60) % 24
+    
+    @staticmethod
+    def posi_distribution(self):
+        # 分布按 'E W T O M' 的顺序记录各区域人数
+        stu_distribution = np.zeros((self.lab_num, 5), dtype=int)
+        tea_distribution = np.zeros((self.lab_num, 5), dtype=int)
+        for s in self.students:
+            stu_distribution[s.lab][s.current_area] += 1
+        for t in self.teachers:
+            tea_distribution[t.lab][t.current_area] += 1
+        return (stu_distribution, tea_distribution)
+
+
+    def __str__(self):
+        print('=============== Simulation ===============')
+        print('[Config]')
+        print('Lab number: {}'.format(self.config.environment.lab_number))
+        print('#Student in each lab: {}'.format(self.stu_num))
+        print('#Teacher in each lab: {}'.format(self.tea_num))
+        print('Lab size: {} x {}'.format(self.lab_size[0], self.lab_size[1]))
+        print('[Virus]')
+        print('Name: {}, Infection R={}'.format(self.virus.name, self.virus.infect_radius))
+
+        print('[State]')
+        print('{} days, {} hours'.format(self.day(self), self.hour(self)))
+        print('Infected people number:')
+        for lab in range(self.lab_num):
+            infected_stu = len([s for s in self.students if s.lab == lab and s.infect_state == 1])
+            infected_tea = len([t for t in self.teachers if t.lab == lab and t.infect_state == 1])
+            print('  Lab {}: student: {} ({}%), teacher: {} ({}%)'.format(lab, infected_stu, round(100*infected_stu / self.stu_num, 2)
+                                                                          , infected_tea, round(100*infected_tea / self.tea_num, 2))
+            )
+        print('People postions:')
+        stu_dis, tea_dis = self.posi_distribution(self)
+        print('(corresponding [E W T O M])')
+        for lab in range(self.lab_num):
+            print('  Lab {}:'.format(lab))
+            print('    Student:', stu_dis[lab])
+            print('    Teacher:', tea_dis[lab])
+        print('==========================================')
+        return ''
 
 
 if __name__ == '__main__':
     _ = parse_args()
     print_easydict(cfg)
     simulation = Simulation(cfg)
+    print(simulation)
--- objects.py
+++ objects.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Virus():
     def __init__(self, name, infect_radius):
         self.name = name
@@ -7,7 +9,7 @@ class Virus():
         return f"Virus({self.name}, {self.infect_radius})"
 
     
-InfectionState = {
+INFECTION_STATE_NAME = {
     0: 'Normal',
     1: 'Infected',
     2: 'Hidden',
@@ -15,7 +17,7 @@ InfectionState = {
     4: 'Recovered',
 }
 
-Area = {
+AREA_FULL_NAME = {
     'E': 'experimental_areas',
     'W': 'work_areas',
     'T': 'toilet',
@@ -23,17 +25,35 @@ Area = {
     'M': 'meeting_room'
 }
 
+AREA = {
+    'E': 0,
+    'W': 1,
+    'T': 2,
+    'O': 3,
+    'M': 4
+}
+
+def exposure_to_prop(a):
+    return a
+
+def probability(time):
+    return 1 / (1 + np.exp(-time))
+
+
 class People():
-    def __init__(self, identity, lab, area, move_matrix, lab_postion=None):
+    def __init__(self, identity: str, identity_id: int, lab: int, area: str, move_matrix, lab_postion=None):
         assert area in ['E', 'W', 'T', 'O', 'M'], "Invalid area: {}".format(area)
         assert identity.lower() in ['student', 'teacher'], "Invalid identity: {}".format(identity)
 
-        self.identity = identity
+        self.identity = identity            # 人员类型
+        self.identity_id = identity_id      # 人员标识符 (全局唯一)
         self.lab = lab
         self.exposure_time = 0
+        self.propagation_capacity = exposure_to_prop(self.exposure_time)
+        self.infected_probability = probability(self.exposure_time)
         self.infect_state = 0   # 0: Normal, 1: Infected, 2: Hidden, 3: Vacation, 4: Recovered
         self.state_duration = 0 # Duration of current state
-        self.current_area = area
+        self.current_area = AREA[area]  # 区域的首字母转换成数字
 
         self.experimental_position = lab_postion  # Position (If in lab)
         self.meeting_state = 0
@@ -46,20 +66,20 @@ class People():
         }
 
     def __str__(self):
-        print(f"{self.identity} at [{Area[self.current_area]}] [State: {InfectionState[self.infect_state]}]")
+        print(f"{self.identity} at [{AREA_FULL_NAME[self.current_area]}] [State: {INFECTION_STATE_NAME[self.infect_state]}]")
 
 
 class Student(People):
-    def __init__(self, lab, area, move_matrix, lab_postion=None):
-        super().__init__('student', lab, area, move_matrix, lab_postion)
+    def __init__(self, identity_id: int, lab: int, area: str, move_matrix, lab_postion=None):
+        super().__init__('student', identity_id, lab, area, move_matrix, lab_postion)
 
     def move(self):
         pass
 
 
 class Teacher(People):
-    def __init__(self, lab, area, move_matrix, lab_postion=None):
-        super().__init__('teacher', lab, area, move_matrix, lab_postion)
+    def __init__(self, identity_id: int, lab: int, area: str, move_matrix, lab_postion=None):
+        super().__init__('teacher', identity_id, lab, area, move_matrix, lab_postion)
 
     def move(self):
         pass
